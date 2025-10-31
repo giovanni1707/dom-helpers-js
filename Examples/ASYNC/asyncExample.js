147d0874d91682e45015f397fb70bc06a4621132
Giovanni <giovannimarianne1@gmail.com>
2025-10-31 09:47:36 +0400

    organize files and folder 311025

--- Examples/ASYNC/asyncExample.js
+++
@@ -1,5 +0,0 @@
-
-const maliciousInput = '<script>alert("test")</script><img src=x onerror=alert(1)>';
-                        const sanitized = Elements.sanitize(maliciousInput);
-                        const isSafe = !sanitized.includes('<script>') && !sanitized.includes('onerror');
-               
